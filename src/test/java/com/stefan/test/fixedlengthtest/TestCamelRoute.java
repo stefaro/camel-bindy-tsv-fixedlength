ef54908a23e4413fb9d951bbb4143ceb9ca4f5a5
Stefan Roos <stefan.h.roos@gmail.com>
2021-03-20 16:45:07 +0200

    Update to Camel 3.8.0

--- src/test/java/com/stefan/test/fixedlengthtest/TestCamelRoute.java
+++ src/test/java/com/stefan/test/fixedlengthtest/TestCamelRoute.java
@@ -7,7 +7,7 @@ import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.RoutesBuilder;
-import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
@@ -18,31 +18,23 @@ import com.google.common.io.Resources;
 
 public class TestCamelRoute extends CamelTestSupport {
 
-	@Produce(uri = "direct:start")
+	@Produce("direct:start")
 	private ProducerTemplate start;
 
-	@EndpointInject(uri = "mock:result")
+	@EndpointInject("mock:result")
 	private MockEndpoint result;
 
 	@Before
 	public void prepareRoute() throws Exception {
-		context.getRouteDefinition("data-route").adviceWith(context, new AdviceWithRouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				replaceFromWith("direct:start");
-				weaveAddLast().to("mock:result");
-			}
+		AdviceWith.adviceWith(context, "data-route", a -> {
+			a.replaceFromWith("direct:start");
+			a.weaveAddLast().to("mock:result");
 		});
 	}
 
 	@Override
-	protected RoutesBuilder createRouteBuilder() throws Exception {
-		return new RouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				includeRoutes(new MyRouteBuilder());
-			}
-		};
+	protected RoutesBuilder[] createRouteBuilders() throws Exception {
+		return new RouteBuilder[] { new MyRouteBuilder() };
 	}
 
 	@Test
